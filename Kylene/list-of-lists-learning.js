15094b74947c5de60d2ee07652ab40cfa13b7977
Nicholas Hare <oharemarketing@gmail.com>
2014-10-04 00:44:28 -0400

    Create list-of-lists-learning.js

---
+++ Kylene/list-of-lists-learning.js
@@ -0,0 +1,79 @@
+//COLLABEDIT LEARNING
+
+for (var i = 0; i < list.length; i++) {
+  // access each item in list
+  var item = list[i]
+  
+  // e.g. if item is a number
+  print "i have " + i + " nicks in prison".
+  
+  // but what if item is not a number? what if item is also a list? so list is a list of lists
+  // you can still treat the variable "item" exactly as you would a normal list
+  for (var j = 0; j < item.length; j++) {
+    var sub_item = item[j]
+  }
+}
+
+
+// more concrete example
+var board = [
+[ 1, 2, 3],
+[ 4, 5, 6],
+[ 7, 8, 9],
+];
+
+// so board is a list of lists
+for (var i = 0; i < board.length; i++) {
+  // access each list in the list of lists
+  var b = board[i]
+  
+  // what is b when i = 0? i = 1? i=2 ?  hint: see the "item" exampel above. remember that "board" is list of lists.
+  var b = board[i] thought? board[1] is the 2nd list for example is it not? where as board [1][1] would be 5  
+
+  [1, 2, 3] is correct. its a list. and the list contains [1,2,3]
+  
+  // so what is b when i=1 and i=2? 
+
+  // now access each item in b
+  for (var j = 0; j < b.length; j++) {
+    var x= b[j];
+    
+    // what is x when i=0 and j=0?  what is x when i=2 and j=1? 
+    x = 1 x = 8 , i think this might be brilliant x/j are the board[2][1] positions im going to have to work with continually on this project i think
+  }
+}
+
+
+var truth = [
+[kylene, is, really, fucking, awesome],
+[he, happens, to, be, amazing],
+[thank, you, so, much, sir],
+];
+
+for(var i = 0; i < truth.length; i++){
+    var b = truth[i];
+    
+    for (var j = 0; j < b.length; j++){
+        var x = b[j];
+        
+    }
+}    
+
+What is x when i = 0 and j = 0? What is x when i = 0 and j 1? What is x when i = 1 and j = 4? What is x when i = 2 and j = 0? What is x when i = 2 and j = 1?
+
+
+var haiku= [
+[this, is, a, hai, ku],
+[here, are, se, ven, syl, la, bles],
+[and , here, are, five, now],
+];
+
+var haiku= [
+[1, 2, 3, 4, 5],
+[6, 7, 8, 9, 10, 11, 12],
+[13, 14, 15, 16, 17],
+];
+
+itsa 
+// so if u think of this as a board
+// its not even rectangular
